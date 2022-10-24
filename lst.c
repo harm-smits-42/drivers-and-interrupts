2f23d6f15647c9a2d81bb0571d9974c98592ecdd
SaltyMilk <soufiane.elmelcaoui@gmail.com>
2022-10-24 05:11:38 +0200

    really, I can't stop making typos

--- lst.c
+++ lst.c
@@ -5,11 +5,11 @@ t_entry_lst *add_entry(t_entry_lst **lst, char *entry)
 	t_entry_lst *new;
 	t_entry_lst *curr = *lst;
 	
-	if (!lst || !(ret = kmalloc(sizeof(t_entry_lst), GFP_ATOMIC)))
+	if (!lst || !(new = kmalloc(sizeof(t_entry_lst), GFP_ATOMIC)))
 		return NULL;
 	
-	new.entry = entry;
-	new.next = NULL;
+	new->entry = entry;
+	new->next = NULL;
 	if (!*lst)
 		*lst = new;
 	else
